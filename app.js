13ba1c077e2990164ea8f1fd74b063b43521dcc0
Kriya <kriya101101@gmail.com>
2024-07-16 19:16:06 +0800

    Implement updating restaurant data

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const port = 3000;
 
 const db = require('./models');
 const restaurantList = db.restaurantList;
+const methodOverride = require('method-override');
 
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
-app.set('views', './views')
+app.set('views', './views');
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req,res) => {
   res.redirect('/restaurants')
@@ -49,11 +51,30 @@ app.get('/restaurants/:id', (req, res) => {
       .catch((err) => console.log(err))
 })
 
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return restaurantList.findByPk(id, {
+    attributes: ['id', 'name', 'name_en', 'category', 'phone', 'image', 'location', 'google_map', 'rating', 'description'],
+    raw: true
+  })
+    .then((restaurant) => res.render('edit', {restaurant}))
+    .catch((err) => console.log(err))
+})
+
 app.post('/restaurants', (req, res) => {
   const data = req.body
   return restaurantList.create({ name:data.name, name_en:data.name_en, category:data.category, phone:data.phone, image:data.image, location:data.location, google_map:data.google_map, rating:data.rating, description:data.description })
     .then(() => res.redirect('/restaurants'))
     .catch((err) => console.log(err)) 
+})
+
+app.put('/restaurants/:id', (req, res) => {
+  const id = req.params.id;
+  const data = req.body
+  return restaurantList.update(
+    {name:data.name, name_en:data.name_en, category:data.category, phone:data.phone, image:data.image, location:data.location, google_map:data.google_map, rating:data.rating, description:data.description}, {where: {id}}
+  )
+    .then(() =>res.redirect(`/restaurants/${id}`))
 })
 
 app.listen(port, () =>{
